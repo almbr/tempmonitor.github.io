ff760ea239dcda63b24d5f74d9af16f45ee5885b
nilssonaxel <axel.a.nilsson@gmail.com>
2016-08-27 14:15:50 +0200

    Instagram widget
    Using instafeed to load instagram pictures. Currently searching for
    #mountains

--- js/theme.js
+++ js/theme.js
@@ -1,3 +1,12 @@
+var userFeed = new Instafeed({
+        get: 'tag',
+        tagName: 'mountains',
+        accessToken: '9f126fc85a6b4578a80c6724983aed5a',
+        limit: 6,
+        target: 'instagram'
+        resolution : 'standard_resolution',
+});
+
 $(document).ready(function(){
 	
 	//Check to see if the window is top if not then display button
@@ -14,6 +23,10 @@ $(document).ready(function(){
 		$('html, body').animate({scrollTop : 0},800);
 		return false;
 	});
+    
+    userFeed.run();
+
+    <script type="text/javascript" src="instafeed.min.js"></script>
     
     $('#constructionModal').modal('show');
     
