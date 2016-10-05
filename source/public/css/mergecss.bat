85bb0f9809b86b578159509f3c7e33ab44e92743
tsuum <erik.almbratt@gmail.com>
2016-10-05 19:49:56 +0200

    Revert "Major update"
    This reverts commit 054211ba136d4468995a2b385966d8abc480b6cc.

--- source/public/css/mergecss.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-del complete.css
-type bootstrap.min.css >> complete.css
-type font-awesome.min.css >> complete.css
-type custom.css >> complete.css
--- source/themes/temp-theme/static/css/mergecss.bat
+++ source/themes/temp-theme/static/css/mergecss.bat
@@ -2,4 +2,4 @@
 del complete.css
 type bootstrap.min.css >> complete.css
 type font-awesome.min.css >> complete.css
-type custom.css >> complete.css
+type style.css >> complete.css
