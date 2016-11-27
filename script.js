639472454e979cb3ece57ddee347ed2b1c383c1c
tsuum <erik.almbratt@gmail.com>
2016-11-27 18:01:05 +0100

    Version2.1 Commit to 2.2

---
+++ script.js
@@ -0,0 +1,65 @@
+  var num = 400; //number of pixels before modifying styles
+
+  $(window).bind('scroll', function () {
+      if ($(window).scrollTop() > num) {
+          $('.menu').addClass('fixed');
+      } else {
+          $('.menu').removeClass('fixed');
+      }
+  });
+
+  var app = angular.module("myApp", ["ngRoute"]);
+  app.config(function($routeProvider) {
+      $routeProvider
+      .when("/", {
+          templateUrl : "main.html"
+      })
+      .when("/shop", {
+          templateUrl : "shop.html"
+      })
+      .when("/about", {
+          templateUrl : "about.html"
+      })
+      .when("/credits", {
+          templateUrl : "credits.html"
+      });
+  });
+
+function toggle(id) {
+  // Style this element
+  stylethis(id);
+
+  //Neutralise other elements
+  if(id == "homeselect") {
+    neutralisethis("shopselect");
+    neutralisethis("aboutselect");
+    neutralisethis("creditsselect");
+  }
+  else if(id == "shopselect") {
+    neutralisethis("homeselect");
+    neutralisethis("aboutselect");
+    neutralisethis("creditsselect");
+  }
+  else if(id == "aboutselect") {
+    neutralisethis("homeselect");
+    neutralisethis("shopselect");
+    neutralisethis("creditsselect");
+  }
+  else if(id == "creditsselect") {
+    neutralisethis("homeselect");
+    neutralisethis("shopselect");
+    neutralisethis("aboutselect");
+  }
+
+  // Scroll to Div
+  $('html, body').animate({
+        scrollTop: $("#anchor").offset().top
+    }, 400);
+}
+
+function stylethis(id) {
+    $("#" + id).addClass("selected");
+}
+function neutralisethis(id) {
+    $("#" + id).removeClass("selected");
+}
